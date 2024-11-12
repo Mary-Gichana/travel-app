7d288db7731f2cb76eac24d6dce9947f5d38662f
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-12 14:27:25 +0300

    useEffect hook fetches and displays data on initial component render

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react'
-import DestinationCard from './DestinationCard'
+import React, { useEffect, useState } from "react";
+import DestinationCard from "./DestinationCard";
 
-function Destinations () {
-  const [destinations, setDestinations] = useState([])
-  return (
-   
-  )
+function Destinations() {
+  const [destinations, setDestinations] = useState([]);
+  return useEffect(() => {
+    fetch("http://localhost:3000/destinations")
+      .then((response) => response.json())
+      .then((data) => setDestinations(data));
+  }, []);
 }
 
-export default Destinations
+export default Destinations;
