178f7a2075535cfb194201539f3fdbf6ced90ddc
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-12 14:41:43 +0300

    added catch method

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -6,9 +6,21 @@ function Destinations() {
   useEffect(() => {
     fetch("http://localhost:3000/destinations")
       .then((response) => response.json())
-      .then((data) => setDestinations(data));
+      .then((data) => setDestinations(data))
+      .catch((error) => console.error(error));
   }, []);
+  return (
+    <ul className="cards">
+      {destinations.map((destination) => (
+        <DestinationCard
+          key={destination.id}
+          name={destination.name}
+          image={destination.image}
+          description={destination.description}
+        />
+      ))}
+    </ul>
+  );
 }
-return;
 
 export default Destinations;
