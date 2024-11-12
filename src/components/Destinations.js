f14840d6abc1c5d689a462ed915f1cf4ccd1e87d
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-12 12:05:34 +0300

    added  useState and useEffect

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DestinationCard from "./DestinationCard";
 
 function Destinations() {
-  return <div>Destinations</div>;
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/destinations")
+      .then((response) => response.json())
+      .then((data) => setDestinations(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <ul className="cards">
+      {destinations.map((destination) => (
+        <DestinationCard
+          key={destination.id}
+          name={destination.name}
+          image={destination.image}
+          description={destination.description}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default Destinations;
