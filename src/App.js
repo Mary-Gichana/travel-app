017d21b5fcb8d139f3d9b4dcfd7083480d338083
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-12 12:28:49 +0300

    added the Destinations component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Destinations from "./components/Destinations.js";
+import Header from "./components/Header.js";
 
 function App() {
-  return <Headers.js />;
+  return (
+    <>
+      <Header />;
+      <Destinations />
+    </>
+  );
 }
 
 export default App;
